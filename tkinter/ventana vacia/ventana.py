70fed6bcb36d89a794799dcb3305ea4ab2ab9794
wgarcia-p <lmg.wgarcia@gmail.com>
2026-02-13 15:24:08 -0600

    tkinter

---
+++ tkinter/ventana vacia/ventana.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Ventana Vacía")
+ventana.geometry("400x300")
+
+# Iniciar el bucle principal
+ventana.mainloop()
