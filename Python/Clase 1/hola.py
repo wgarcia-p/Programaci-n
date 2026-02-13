7e37d2566db88f28e3a33c7561c0d15d4110b40b
wgarcia-p <lmg.wgarcia@gmail.com>
2026-02-13 14:24:47 -0600

    Mi primer commit

---
+++ Python/Clase 1/hola.py
@@ -0,0 +1,7 @@
+print("Hola mundo")
+print("Bienvenidos a la clase de Python")
+print("Espero que disfruten aprendiendo Python")
+# Este es un comentario en Python
+# Los comentarios son útiles para explicar el código y hacerlo más legible
+# También puedes usar comentarios para desactivar temporalmente partes del código
+# print("Este código no se ejecutará porque está comentado")
