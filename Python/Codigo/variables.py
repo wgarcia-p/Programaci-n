f2899db25d545b93f47870a1afdd46aec0c5ffa7
wgarcia-p <lmg.wgarcia@gmail.com>
2026-02-13 14:39:21 -0600

    Cambio 3

---
+++ Python/Codigo/variables.py
@@ -0,0 +1,23 @@
+import os
+
+def clrscr():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clrscr()
+
+
+edad= 25
+nombre= "Juan"
+altura= 1.75
+print("Mi nombre es", nombre, "y tengo", edad, "años.")
+print("Mi altura es", altura, "metros.")
+# Puedes realizar operaciones con variables
+suma = edad + 5
+print("En 5 años tendré", suma, "años.")
+# También puedes usar variables para almacenar resultados de operaciones
+multiplicacion = edad * 2
+print("El doble de mi edad es", multiplicacion, "años.")
+# Las variables pueden ser de diferentes tipos: enteros, flotantes, cadenas, etc.
+# Es importante elegir nombres de variables descriptivos para mejorar la legibilidad del código
+# Por ejemplo, en lugar de usar "x" como nombre de variable, es mejor usar "edad" o "nombre" para que sea más claro lo que representa la variable.
+
