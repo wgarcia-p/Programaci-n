b3c8f8bc3dd75083a1970b993fcd572ca3e2bbfa
wgarcia-p <lmg.wgarcia@gmail.com>
2026-02-13 15:11:40 -0600

    cambio nombres

--- 05problema1_for.py
+++ 05problema1_for.py
@@ -4,6 +4,7 @@
 # input siempre devuelve un string, por lo que es necesario convertirlo a entero usando int() para poder realizar operaciones matemáticas
 numero = int(input("Ingrese un número para mostrar su tabla de multiplicar: "))
 print("Tabla de multiplicar del", numero)
+# se usa un numero mayor a 1 para incluir el numero ingresado en la tabla de multiplicar, y un numero menor a 11 para incluir el numero 10 en la tabla de multiplicar
 for i in range(1, 11):
     resultado = numero * i
     print(numero, "x", i, "=", resultado)
